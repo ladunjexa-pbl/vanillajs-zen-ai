55d165621c8fe354d92583807518c54405297271
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2022-12-27 06:56:24 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://zenai.onrender.com/", {
+  const response = await fetch("https://zenai.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
